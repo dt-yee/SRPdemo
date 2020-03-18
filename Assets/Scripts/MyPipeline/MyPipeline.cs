b947f982afa63f95d4af2276c52ac2b61686cbf9
unknown <yidaitian@shengqugames.com>
2020-03-18 18:31:49 +0800

    point and spot light

--- Assets/Scripts/MyPipeline/MyPipeline.cs
+++ Assets/Scripts/MyPipeline/MyPipeline.cs
@@ -13,13 +13,18 @@ public class MyPipeline : RenderPipeline
     const int maxVisibleLights = 4;
 
     static int visibleLightColorsId = Shader.PropertyToID("_VisibleLightColors");
-    static int visibleLightDirectionsId = Shader.PropertyToID("_VisibleLightDirections");
+    static int visibleLightDirectionsOrPositionsId = Shader.PropertyToID("_VisibleLightDirectionsOrPositions");
+    static int visibleLightAttenuationsId = Shader.PropertyToID("_VisibleLightAttenuations");
+    static int visibleLightSpotDirectionsId = Shader.PropertyToID("_VisibleLightSpotDirections");
 
     Vector4[] visibleLightColors = new Vector4[maxVisibleLights];
-    Vector4[] visibleLightDirections = new Vector4[maxVisibleLights]; 
+    Vector4[] visibleLightDirections = new Vector4[maxVisibleLights];
+    Vector4[] visibleLightAttenuations = new Vector4[maxVisibleLights];
+    Vector4[] visibleLightSpotDirections = new Vector4[maxVisibleLights];
 
     public MyPipeline (bool dynamicBatching, bool instancing)
     {
+        GraphicsSettings.lightsUseLinearIntensity = true;
         if (dynamicBatching)
         {
             drawflags = DrawRendererFlags.EnableDynamicBatching;
@@ -36,11 +41,11 @@ public class MyPipeline : RenderPipeline
         //renderContext.Submit();
         foreach (var camera in cameras)
         {
-            Render(renderContext, camera);
+            RenderSingle(renderContext, camera);
         }
     }
 
-    void Render (ScriptableRenderContext context, Camera camera)
+    void RenderSingle (ScriptableRenderContext context, Camera camera)
     {
         ScriptableCullingParameters cullingParameters;
         if(!CullResults.GetCullingParameters(camera, out cullingParameters))
@@ -69,10 +74,15 @@ public class MyPipeline : RenderPipeline
             (clearFlags & CameraClearFlags.Color) != 0,
             camera.backgroundColor
         );
+
+        ConfigureLights();
+
         buffer.BeginSample("Render Camera");
 
         buffer.SetGlobalVectorArray(visibleLightColorsId, visibleLightColors);
-        buffer.SetGlobalVectorArray(visibleLightDirectionsId, visibleLightDirections);
+        buffer.SetGlobalVectorArray(visibleLightDirectionsOrPositionsId, visibleLightDirections);
+        buffer.SetGlobalVectorArray(visibleLightAttenuationsId, visibleLightAttenuations);
+        buffer.SetGlobalVectorArray(visibleLightSpotDirectionsId, visibleLightSpotDirections);
 
         //buffer.ClearRenderTarget(true, false, Color.clear);
         context.ExecuteCommandBuffer(buffer);
@@ -104,6 +114,60 @@ public class MyPipeline : RenderPipeline
 
         context.Submit();
     }
+
+    void ConfigureLights()
+    {
+        for(int i =0; i<cull.visibleLights.Count; ++i)
+        {
+            if (i == maxVisibleLights)
+                break;
+
+            VisibleLight light = cull.visibleLights[i];
+            visibleLightColors[i] = light.finalColor;
+            Vector4 attenuation = Vector4.zero;
+            attenuation.w = 1f;
+            Vector4 v;
+            if (light.lightType == LightType.Directional)
+            {
+                v = light.localToWorld.GetColumn(2);
+                v.x = -v.x;
+                v.y = -v.y;
+                v.z = -v.z;
+            }
+            else
+            {
+                v = light.localToWorld.GetColumn(3);
+                attenuation.x = 1.0f / Mathf.Max(light.range * light.range, 1e-3f);
+
+                if(light.lightType == LightType.Spot)
+                {
+                    Vector4 v1;
+                    v1 = light.localToWorld.GetColumn(2);
+                    v1.x = -v1.x;
+                    v1.y = -v1.y;
+                    v1.z = -v1.z;
+                    visibleLightSpotDirections[i] = v1;
+                    float outerRad = Mathf.Deg2Rad * 0.5f * light.spotAngle;
+                    float outerCos = Mathf.Cos(outerRad);
+                    float outerTan = Mathf.Tan(outerRad);
+
+                    float innerCos = Mathf.Cos(Mathf.Atan(23f / 32f) * outerTan);
+                    float angleRange = Mathf.Max(innerCos - outerCos, 1e-3f);
+                    attenuation.z = 1f / angleRange;
+                    attenuation.w = -outerCos * attenuation.z;
+                }
+            }
+            visibleLightAttenuations[i] = attenuation;
+            visibleLightDirections[i] = v;
+
+        }
+        for(int i = maxVisibleLights-1; i >= cull.visibleLights.Count; --i)
+        {
+            visibleLightColors[i] = Color.clear;
+        }
+
+    }
+
 
     [Conditional("DEVELOPMENT_BUILD"), Conditional("UNITY_EDITOR")]
     void DrawDefaultPipeline(ScriptableRenderContext context, Camera camera)
--- Assets/Scripts/ShaderLab/Lit.hlsl
+++ Assets/Scripts/ShaderLab/Lit.hlsl
@@ -14,7 +14,9 @@ CBUFFER_END
 
 CBUFFER_START(_LightBuffer)
     float4 _VisibleLightColors[MAX_VISIBLE_LIGHTS];
-    float4 _VisibleLightDirections[MAX_VISIBLE_LIGHTS];
+    float4 _VisibleLightDirectionsOrPositions[MAX_VISIBLE_LIGHTS];
+    float4 _VisibleLightAttenuations[MAX_VISIBLE_LIGHTS];
+    float4 _VisibleLightSpotDirections[MAX_VISIBLE_LIGHTS];
 CBUFFER_END
 
 //CBUFFER_START(UnityPerMaterial)
@@ -28,11 +30,30 @@ UNITY_DEFINE_INSTANCED_PROP(float4, _Color)
 UNITY_INSTANCING_BUFFER_END(PerInstance)
 
 
-float3 DiffuseLight(int index, float3 normal)
+float3 DiffuseLight(int index, float3 normal, float3 worldPos)
 {
-    float lightColor = _VisibleLightColors[index].rgb;
-    float lightDir = _VisibleLightColors[index].xyz;
+    float3 lightColor = _VisibleLightColors[index].rgb;
+    float4 lightData = _VisibleLightDirectionsOrPositions[index];
+    float3 lightVector = lightData.xzy - worldPos * lightData.w;
+    float4 spotDirection = _VisibleLightSpotDirections[index];
+
+    float3 lightDir = normalize(lightVector);
     float diffuse = saturate(dot(normal, lightDir));
+
+    float4 lightAtt = _VisibleLightAttenuations[index];
+
+    float dis = dot(lightDir, lightDir);
+
+    float rangeFade = dis * lightAtt.x;
+    rangeFade = saturate(1-rangeFade*rangeFade);
+    rangeFade *= rangeFade;
+
+    float spotFade = dot(spotDirection, lightDirection);
+    spotFade = saturate(spotFade * lightAtt.z + lightAtt.w);
+    spotFade *= spotFade;
+
+
+    diffuse *= rangefade * spotFade / max(dis, 1e-3f);
     return diffuse * lightColor;
 }
 
@@ -45,6 +66,7 @@ struct VertexInput{
 struct VertexOutput{
     float4 clipPos : SV_POSITION;
     float3 normal : TEXCOORD0;
+    float3 worldPos : TEXCOORD1;
     UNITY_VERTEX_INPUT_INSTANCE_ID
 
 };
@@ -56,6 +78,7 @@ VertexOutput LitPassVertex(VertexInput input){
     float4 worldPos = mul(UNITY_MATRIX_M, float4(input.pos.xyz, 1.0));
     output.clipPos = mul(unity_MatrixVP, worldPos);
     output.normal = mul((float3x3)UNITY_MATRIX_M, input.normal);
+    output.worldPos = worldPos;
     return output;
 }
 
@@ -65,9 +88,9 @@ float4 LitPassFragment(VertexOutput input) :SV_TARGET{
     input.normal = normalize(input.normal);
     float3 albedo = UNITY_ACCESS_INSTANCED_PROP(PerInstance, _Color).rgb;
 
-    float diffuseLight = 0;
+    float3 diffuseLight = 0;
     for (int i = 0; i < MAX_VISIBLE_LIGHTS; ++i)
-        diffuseLight += DiffuseLight(i,input.normal);
+        diffuseLight += DiffuseLight(i, input.normal, input.worldPos);
 
     float3 color = diffuseLight * albedo;
     return float4(color, 1.0);
